394b43c9aa8e7227856cc97221935911e815c23e
Maksymilian Wojnar <maksymilian.wojnar@outlook.com>
2024-11-15 09:15:36 +0100

    Add a plot script to dynamic scenario

---
+++ mldr/plots/dynamic_plot.py
@@ -0,0 +1,57 @@
+from glob import glob
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+from mldr.plots.config import get_cmap, AGENT_NAMES, COLUMN_WIDTH, COLUMN_HIGHT
+
+
+if __name__ == '__main__':
+    min_wifi = 5
+    n_wifi = 50
+    interval_sta = 5
+    interval_time = 100
+    n_steps = (n_wifi - min_wifi) // interval_sta + 1
+
+    cmap = get_cmap(n=len(AGENT_NAMES))
+    agent_results = {}
+
+    for file in glob(f'../../scripts/dynamic_*_m*_n*_s*.csv'):
+        df = pd.read_csv(file)
+        agent = file.split('_')[1:3]
+        agent = '_'.join(agent) if agent[1] == 'RTS' else agent[0]
+        agent = AGENT_NAMES[agent]
+        agent_results[agent] = df.iloc[1:, -2].values.reshape(-1, 2).mean(axis=1)
+
+    _, ax = plt.subplots(figsize=(2 * COLUMN_WIDTH, COLUMN_HIGHT))
+
+    for i in range(1, n_steps):
+        ax.axvline(i * interval_time, color='red', linewidth=0.5, linestyle='--')
+
+    for i, (agent, results) in enumerate(sorted(agent_results.items())):
+        xs = np.linspace(0, interval_time * n_steps, len(results))
+        ax.plot(xs, results, color=cmap[i], label=agent)
+
+    ax2 = ax.twinx()
+    x = sum([2 * [i * interval_time] for i in range(n_steps + 1)], [])[1:-1]
+    y = sum([2 * [min_wifi + i * interval_sta] for i in range(n_steps)], [])
+    ax2.plot(x, y, color='black', linewidth=0.5)
+    ax2.set_ylim(0, n_wifi)
+    ax2.set_yticks(range(0, n_wifi + 1, 2 * interval_sta))
+    ax2.set_ylabel('Number of devices')
+
+    ax.set_xlabel('Time [s]')
+    ax.set_ylabel('Throughput [Mb/s]')
+    ax.set_ylim(0, 125)
+    ax.set_xlim(0, xs[-1])
+    ax.set_xticks(range(0, int(xs[-1]) + 1, 100))
+    ax.grid()
+
+    ax.plot([None], [None], color='black', linewidth=0.5, label='Number of devices')
+    ax.plot([None], [None], color='r', linewidth=0.5, linestyle='--', label='Reset agent')
+    ax.legend(ncol=2, bbox_to_anchor=(0.52, 0.18), loc='center')
+
+    plt.tight_layout()
+    plt.savefig(f'dynamic.pdf', bbox_inches='tight')
+    plt.show()
--- scripts/run_dynamic.sh
+++ scripts/run_dynamic.sh
@@ -4,8 +4,8 @@ SEED=100
 MIN_WIFI=5
 N_WIFI=50
 INTERVAL_STA=5
-INTERVAL_TIME=50
-SIMULATION_TIME=500
+INTERVAL_TIME=100
+SIMULATION_TIME=1000
 
 python $MLDR_DIR/mldr/envs/run.py --urllc=0 --throughput=1 --massive=0 --agentName="MLDR" --nWifi=$N_WIFI --minWifi=$MIN_WIFI --intervalSta=$INTERVAL_STA --intervalTime=$INTERVAL_TIME --dataRate=120 --seed=$SEED --simulationTime=$SIMULATION_TIME --csvLogPath="$MLDR_DIR/scripts/dynamic_MLDR_m${MIN_WIFI}_n${N_WIFI}_s${SEED}.csv"
 python $MLDR_DIR/mldr/envs/run.py --urllc=0 --throughput=1 --massive=0 --agentName="wifi" --nWifi=$N_WIFI --minWifi=$MIN_WIFI --intervalSta=$INTERVAL_STA --intervalTime=$INTERVAL_TIME --dataRate=120 --seed=$SEED --simulationTime=$SIMULATION_TIME --csvLogPath="$MLDR_DIR/scripts/dynamic_80211_m${MIN_WIFI}_n${N_WIFI}_s${SEED}.csv"
